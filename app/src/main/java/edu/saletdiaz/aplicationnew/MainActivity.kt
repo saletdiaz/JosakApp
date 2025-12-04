ef78b7e3af4c0cb4805477fca0eff91b3046f8ff
Kaxi <liukaxi0413@gmail.com>
2025-12-04 18:56:45 +0100

    Añadir el SplashActivity.kt

--- app/src/main/java/edu/saletdiaz/aplicationnew/MainActivity.kt
+++ app/src/main/java/edu/saletdiaz/aplicationnew/MainActivity.kt
@@ -10,6 +10,7 @@ import edu.saletdiaz.aplicationnew.ui.navigation.NavigationHost
 import edu.saletdiaz.aplicationnew.ui.theme.AplicationNewTheme
 import edu.saletdiaz.aplicationnew.ui.view.HomeScreen
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/edu/saletdiaz/aplicationnew/SplashActivity.kt
@@ -0,0 +1,17 @@
+package edu.saletdiaz.aplicationnew
+
+import android.content.Intent
+import android.os.Bundle
+import android.app.Activity
+
+class SplashActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Pager)
+        super.onCreate(savedInstanceState)
+
+        android.os.Handler(android.os.Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
